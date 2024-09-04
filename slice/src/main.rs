a467cf3283ee81464e07512e4bf6324cc983a63a
hyeondobin <dobinhyeon@gmail.com>
2024-09-04 23:54:51 +0900

    slice

---
+++ slice/src/main.rs
@@ -0,0 +1,16 @@
+fn get_first_word(s: String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
+
+fn main() {
+    let mut s = String::from("hello world!");
+    let word = get_first_word(s);
+}
